69b1a42aea3748b54011966b2fd4115d675231f5
noland <mynameisnoland@gmail.com>
2025-02-20 13:22:24 -0500

    updated app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,16 +1,13 @@
-# newest version
-
 import streamlit as st
 
 from icons.icons import toolbox_icon
-from components.initialize_state import initialize_state
+from components.custom_tabs import tabs
 from components.user_credentials import show_user_credentials
+from components.establishment_search import establishment_search
 from components.search_area import show_search_area
 from components.query_options import show_query_options
 from components.review_query import review_and_submit_query
-from components.establishment_search import establishment_search
 
-from components.auto_scroller import scroll_to_top_of_map
 
 st.set_page_config(
    page_title="Tactical OPE Toolkit", 
@@ -18,30 +15,21 @@ st.set_page_config(
    layout="wide"
 )
 
-initialize_state()
+st.subheader('Tactical OPE Toolkit') 
 
-# def test_callback():
-#     if st.session_state['navbar'] == 'User Credentials':
-#         scroll_to_top_of_map()
+# define the tabs you want displayed
+active_tab = tabs(['User Credentials', 'Search Area', 'Query Options', 'Review + Submit'])
 
-with st.sidebar:
-    st.subheader('Tactical OPE Toolkit')   
-    st.radio("Navigation", ['User Credentials', 'Search Area', 'Query Options', 'Review + Submit'], label_visibility='collapsed', key='navbar')
-    # st.write(st.session_state)
-    
-st.subheader('User Credentials')
-show_user_credentials()
-st.divider() 
+# checking and conditionally rendering based on what tab is selected
+if st.session_state['active_tab'] == 'User Credentials':
+      show_user_credentials()
+elif st.session_state['active_tab'] == 'Search Area':
+      establishment_search()
+      show_search_area()
+elif st.session_state['active_tab'] == 'Query Options':
+      show_query_options()
+elif st.session_state['active_tab'] == 'Review + Submit':
+      review_and_submit_query()
 
-st.subheader('Search Area')
-establishment_search()
-show_search_area()
-st.divider() 
 
-st.subheader('Query Options')
-show_query_options()
-st.divider() 
-
-st.subheader('Review + Submit')
-review_and_submit_query()
-
+# st.write(st.session_state)
