e01b3bf676148541a5e594c65c83a5386dbdd19e
noland <104016970+xnolandx@users.noreply.github.com>
2025-03-12 14:15:26 -0400

    User can no longer spam dropping the same box

--- frontend/tabs/dev_search_area.py
+++ frontend/tabs/dev_search_area.py
@@ -3,7 +3,6 @@
 # user bbox2: -14.445651, -14.911131 | 29.678209, 33.604494
 
 # TODO:
-# still able to double tap user dropped boxes
 # zoom shenanigans
 # change how your deleting the pins to match how youre deleting the boxes?
 # legend for the map signifying red is active box
@@ -14,12 +13,30 @@ import folium
 import streamlit as st
 from streamlit_folium import st_folium
 from folium.plugins import Draw
+import math
 
 from components.validation_functions import validate_location
 
 def generate_map():
     m = folium.Map(location=st.session_state['location_validation_results'], zoom_start=st.session_state['map_zoom_level'])
     return m
+
+def check_for_duplicate_boxes(bounds):
+    # value altered and returned at end after comparison
+    already_exists = False
+
+    # Convert bounds to a list of lists if it's a list of tuples
+    bounds = [list(bound) for bound in bounds]
+
+    for child in st.session_state['rectangle_feature_group']._children.values():
+            if isinstance(child, folium.Rectangle):
+                child_bounds = child.get_bounds()
+
+                if bounds == child_bounds:
+                    already_exists = True
+                    break
+
+    return already_exists
 
 @st.fragment
 def search_area():
@@ -93,34 +110,40 @@ def search_area():
                     # if the input coords are validated:
                     if st.session_state['validated_sw_coord'] and st.session_state['validated_ne_coord']:
 
-                        # create the box and add it to the rectangles feature group
-                        folium.Rectangle(
-                            bounds=[(st.session_state['validated_sw_coord']), (st.session_state['validated_ne_coord'])], 
-                            color='red', 
-                            fill=True
-                        ).add_to(st.session_state['rectangle_feature_group'])
+                        # put the coordinates into the correct format for duplication checking and creating the feature
+                        bounds = [(st.session_state['validated_sw_coord']), (st.session_state['validated_ne_coord'])]
 
-                        # edit the "last_active_drawing" in session state to reflect the new box
-                        sw_lat, sw_lon = st.session_state['validated_sw_coord']
-                        ne_lat, ne_lon = st.session_state['validated_ne_coord']
-                        
-                        new_bounding_box = {
-                            "type":"Feature",
-                            "properties":{},
-                            "geometry":{
-                                "type":"Polygon",
-                                "coordinates":[[
-                                                [sw_lon, sw_lat],  # SW corner 
-                                                [sw_lon, ne_lat],  # NW corner 
-                                                [ne_lon, ne_lat],  # NE corner 
-                                                [ne_lon, sw_lat],  # SE corner 
-                                                [sw_lon, sw_lat]   # Closing back at SW corner (Southwest)
-                                                ]]}}
-                        
-                        st.session_state['map']['last_active_drawing'] = new_bounding_box 
+                        # ensure a box with those coordinates doesn't already exist
+                        if not check_for_duplicate_boxes(bounds):
 
-                        # zoom to the box
-                        st.session_state['map_zoom_level'] = 5
+                            # create the box and add it to the rectangles feature group
+                            folium.Rectangle(
+                                bounds=bounds, 
+                                color='red', 
+                                fill=True
+                            ).add_to(st.session_state['rectangle_feature_group'])
+
+                            # edit the "last_active_drawing" in session state to reflect the new box
+                            sw_lat, sw_lon = st.session_state['validated_sw_coord']
+                            ne_lat, ne_lon = st.session_state['validated_ne_coord']
+                            
+                            new_bounding_box = {
+                                "type":"Feature",
+                                "properties":{},
+                                "geometry":{
+                                    "type":"Polygon",
+                                    "coordinates":[[
+                                                    [sw_lon, sw_lat],  # SW corner 
+                                                    [sw_lon, ne_lat],  # NW corner 
+                                                    [ne_lon, ne_lat],  # NE corner 
+                                                    [ne_lon, sw_lat],  # SE corner 
+                                                    [sw_lon, sw_lat]   # Closing back at SW corner (Southwest)
+                                                    ]]}}
+                            
+                            st.session_state['map']['last_active_drawing'] = new_bounding_box 
+
+                            # zoom to the box
+                            st.session_state['map_zoom_level'] = 5
                     
                     
 
@@ -167,13 +190,10 @@ def search_area():
 
                 # Check if this is the last active drawing and set color to red, else blue
                 if not already_exists:
-                    # color = 'red' if coords == st.session_state['map']['last_active_drawing']['geometry']['coordinates'] else 'blue'
-                    
                     folium.Rectangle(
                         bounds=bounds, 
                         color='blue', 
                         fill=True
-                    # ).add_to(m)
                     ).add_to(st.session_state['rectangle_feature_group'])
 
                     st.rerun() # rerenders the map so that a newly drawn shape is immediately being tracked
@@ -198,6 +218,7 @@ def search_area():
         st_folium(m, 
                   feature_group_to_add=[st.session_state['points_feature_group'], st.session_state['rectangle_feature_group']],
                   width=600, height=350, key='map', use_container_width=True, returned_objects=['last_active_drawing'])
+        
 
     # # Display the last active drawing coordinates
     # try:
