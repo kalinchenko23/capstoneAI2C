6ccb252390d67fd0de204a464152b00294ca9c90
noland <104016970+xnolandx@users.noreply.github.com>
2025-03-25 21:03:39 -0400

    removed user_id and token input state variables. added api key (google maps, llm, vlm) state variables. General organization

--- frontend/state_management/persist_state.py
+++ frontend/state_management/persist_state.py
@@ -6,8 +6,8 @@ import streamlit as st
 # by adding this to the callback function, input values persist across tab changes
 # this is absolutely necessary if we keep the faux tabs
 def persist_state():
-    st.session_state['user_id'] = st.session_state['user_id']
-    st.session_state['token_input'] = st.session_state['token_input']
+    # search area state variables
+    st.session_state['establishment_search_input'] = st.session_state['establishment_search_input']
     st.session_state['location_input'] = st.session_state['location_input']
     st.session_state['location_type'] = st.session_state['location_type']
     st.session_state['location_validation_results'] = st.session_state['location_validation_results']
@@ -17,15 +17,19 @@ def persist_state():
     st.session_state['map_zoom_level'] = st.session_state['map_zoom_level']
     st.session_state['points_feature_group'] = st.session_state['points_feature_group']
     st.session_state['rectangle_feature_group'] = st.session_state['rectangle_feature_group']
-    st.session_state['establishment_search_input'] = st.session_state['establishment_search_input']
     st.session_state['map'] = st.session_state['map']
     st.session_state['user_bounding_box'] = st.session_state['user_bounding_box']
-    st.session_state['vlm_input'] = st.session_state['vlm_input']
+
+    # query options state variables
     st.session_state['all_fields_checkbox'] = st.session_state['all_fields_checkbox']
     st.session_state['basic_data_checkbox'] = st.session_state['basic_data_checkbox']
+    st.session_state['google_maps_api_key'] = st.session_state['google_maps_api_key']
     st.session_state['include_reviews_checkbox'] = st.session_state['include_reviews_checkbox']
+    st.session_state['llm_key'] = st.session_state['llm_key']
     st.session_state['include_photo_captioning_checkbox'] = st.session_state['include_photo_captioning_checkbox']
-    st.session_state['kml_download_option'] = st.session_state['kml_download_option']
+    st.session_state['vlm_key'] = st.session_state['vlm_key']
+    st.session_state['vlm_input'] = st.session_state['vlm_input']
+    st.session_state['kmz_download_option'] = st.session_state['kmz_download_option']
 
 
 # Ensures the code runs only when this file is executed directly
