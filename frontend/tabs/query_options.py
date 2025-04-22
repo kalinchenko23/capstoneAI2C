b5873b784b940885f14c410b29cd3833fb2b0a42
noland <104016970+xnolandx@users.noreply.github.com>
2025-04-22 16:44:51 -0400

    added the time and cost estimator widget

--- frontend/tabs/query_options.py
+++ frontend/tabs/query_options.py
@@ -2,11 +2,9 @@ import streamlit as st
 import pandas as pd
 
 from components.cost_estimator import cost_estimator
-from components.cost_estimator import mock_cost_estimator
+from styles.icons.icons import no_results_icon
 
-# TODO:
-
-
+# the backend is returning times in seconds, this formats them to be more readable
 def format_duration(seconds):
     seconds_in_minute = 60
     seconds_in_hour = 3600
@@ -17,7 +15,7 @@ def format_duration(seconds):
     minutes = remaining_seconds // seconds_in_minute
     leftover_seconds = remaining_seconds % seconds_in_minute
 
-    # Optional: round up minutes only if we already have at least 1 minute or hour
+    # round up minutes only if we already have at least 1 minute or hour
     if leftover_seconds and (minutes > 0 or hours > 0):
         minutes += 1
 
@@ -33,10 +31,10 @@ def format_duration(seconds):
 
 @st.fragment
 def query_options():
-    test_col_1, test_col_2 = st.columns(2)
+    query_options_col, price_prediction_col = st.columns(2)
 
     # creates the "select output fields" containers and pills
-    with test_col_1.container(border=True, key='select_output_fields_container'):
+    with query_options_col.container(border=True, key='select_output_fields_container'):
         col_1, col_2, col_3 = st.columns(3) # this is just an easy way to move the toggle further to the right
         col_1.write('Select the Data Included in Output')
         # creates the kmz toggle
@@ -104,12 +102,6 @@ def query_options():
                         label_visibility="collapsed"
                     )
 
-
-
-
-
-
-
         # ai review summary checkbox
         with st.container(border=True):
             review_col_1, review_col_2 = st.columns(2)
@@ -170,39 +162,36 @@ def query_options():
                     )
 
     # beginning of cost prediction stuff
-    
-    with test_col_2.container(border=True):
-        # st.write(st.session_state['price_prediction'])
-        # st.write(st.session_state['price_predicted'])
+    with price_prediction_col.container(border=True):
 
-        if st.session_state['price_predicted'] == False:
-
-            precondtions_prompt = ('In order to make a prediction you must provide:\n'
-                                    '1. Establishment Search Term\n'
-                                    '2. Search Area Bounding Box\n'
-                                    '3. Google Maps API Key'
-                                    )
-            
-            prediction_cost_prompt = "Predicting the cost will incur a charge between 3 and 9 cents."
+        # this state var is instantiated as False and changes to True when the Price Prediction button is clicked
+        if st.session_state['price_predicted'] == False:  
 
             # three conditions need to be met for the price prediction button to be enabled
             predict_cost_preconditions = bool(st.session_state['establishment_search_input'] and 
                                          st.session_state['user_bounding_box'] and 
                                          st.session_state['google_maps_api_key'])
-            
+
+            # prompt thats used before a price has been predicted if any of these havent been provided 
+            precondtions_prompt = ('In order to make a prediction you must provide:\n'
+                                    '1. Establishment Search Term\n'
+                                    '2. Search Area Bounding Box\n'
+                                    '3. Google Maps API Key'
+                                    )
+            # prompt thats used before a price has been predicted if all three preconditons are provided
+            prediction_cost_prompt = "Predicting the cost will incur a charge between 3 and 9 cents."
+
+            # if the button is clicked, call the cost estimator function
             if st.button('Predict Query Time and Cost', 
-                            help=prediction_cost_prompt if predict_cost_preconditions else precondtions_prompt, 
-                            on_click=None, 
+                            help=prediction_cost_prompt if predict_cost_preconditions else precondtions_prompt, # determines which prompt to display 
                             type="secondary", 
-                            disabled=False if predict_cost_preconditions else True, 
+                            disabled=False if predict_cost_preconditions else True,
                             use_container_width=True):
                 
                 with st.spinner():
-                    # cost_estimator()
-                    mock_cost_estimator()
-                    st.session_state['price_predicted'] = True
-                    st.rerun(scope='fragment')
+                    cost_estimator()
             
+            # generic empty table data
             table_data = [
                             {"Tier": "Basic",  "Time": "--",  "Cost": "--"},
                             {"Tier": "Review", "Time": "--",  "Cost": "--"},
@@ -211,75 +200,88 @@ def query_options():
                         ]
             
             df = pd.DataFrame(table_data)
-            df.reset_index(drop=True)
+
+            # display generic empty table
             st.write('establishments found: -')
             st.table(df)
             
-        
+        # if the button has been pressed and the user tried to generate a prediction
         elif st.session_state['price_predicted'] == True:
             st.button('Predict Query Time and Cost', 
                         help="Price already predicted for current search term and search area",  
                         type="secondary", 
-                        disabled=True, 
+                        disabled=True, # disables the button (to prevent the user from spamming the same prediction)
                         use_container_width=True)
             
-            data = st.session_state['price_prediction']
-            table_data = [
-                            {"Tier": "Basic",  "Time": format_duration(data["basic_time"]),      "Cost": data["basic_cost"]},
-                            {"Tier": "Review", "Time": format_duration(data["reviews_time"]),    "Cost": data["reviews_cost"]},
-                            {"Tier": "Photo",  "Time": format_duration(data["photos_time"]),     "Cost": data["photos_cost"]},
-                            {"Tier": "All",    "Time": format_duration(data["time_everything"]), "Cost": data["cost_everything"]}
+            # this state variable is instantiated as an empty dict. when the cost_estimator call is made and there are relevant results
+            # returned, that dict gets filled with the return from the backend. 
+            if st.session_state['price_prediction']:
+                data = st.session_state['price_prediction']
+
+                table_data = [
+                                {"Tier": "Basic",  "Time": format_duration(data["basic_time"]),      "Cost": data["basic_cost"]},
+                                {"Tier": "Review", "Time": format_duration(data["reviews_time"]),    "Cost": data["reviews_cost"]},
+                                {"Tier": "Photo",  "Time": format_duration(data["photos_time"]),     "Cost": data["photos_cost"]},
+                                {"Tier": "All",    "Time": format_duration(data["time_everything"]), "Cost": data["cost_everything"]}
+                            ]
+
+                df = pd.DataFrame(table_data)
+
+                # Round all numeric values returned from the backend
+                df["Cost"] = df["Cost"].apply(lambda x: f"{x:.2f}" if isinstance(x, (int, float)) else x)
+                df["Time"] = df["Time"].apply(lambda x: f"{x:.2f}" if isinstance(x, (int, float)) else x)
+                
+                # Display Results
+                st.write(f'Establishments Found: {data["places"]}')
+                st.table(df)
+
+                # set state variable tracking if a prediction has been generated
+                st.session_state['price_predicted'] = True
+
+                # get the sums of currently selected tiers to display to the user
+                total_prediction = st.session_state['price_prediction']
+                time_prediction = 0
+                cost_prediction = 0
+
+                if st.session_state['basic_data_checkbox']:
+                    time_prediction += total_prediction['basic_time']
+                    cost_prediction += total_prediction['basic_cost']
+                if st.session_state['include_reviews_checkbox']:
+                    time_prediction += total_prediction['reviews_time']
+                    cost_prediction += total_prediction['reviews_cost']
+                if st.session_state['include_photo_captioning_checkbox']:
+                    time_prediction += total_prediction['photos_time']
+                    cost_prediction += total_prediction['photos_cost']
+                
+                time_prediction = round(time_prediction, 2)
+                cost_prediction = round(cost_prediction, 2)
+
+                query_time_prediction_col, query_cost_prediction_col = st.columns(2)
+
+                # although these are being used below, Im putting them in state so that they can be displayed
+                # on the 'Review + Submit' tab. (This avoids having to format/round the same numbers twice)
+                st.session_state['predicted_time'] = f'{format_duration(time_prediction)}'
+                st.session_state['predicted_cost'] = f'${cost_prediction:.2f}'
+
+                query_time_prediction_col.write(f'Predicted Query Time: {st.session_state['predicted_time']}')
+                query_cost_prediction_col.write(f'Predicted Query Cost: {st.session_state['predicted_cost']}')
+
+            else: #'price_prediction' has been generated but there were no results in the query from google
+                st.error(f'No results found for "{st.session_state['establishment_search_input']}" within your bounding box.', icon=no_results_icon)
+
+                table_data = [
+                            {"Tier": "Basic",  "Time": "--",  "Cost": "--"},
+                            {"Tier": "Review", "Time": "--",  "Cost": "--"},
+                            {"Tier": "Photo",  "Time": "--",  "Cost": "--"},
+                            {"Tier": "All",    "Time": "--",  "Cost": "--"}
                         ]
-            df = pd.DataFrame(table_data)
-            df.reset_index(drop=True)
-
-            # Round all numeric values (except for "couple of seconds", which is a string)
-            df["Cost"] = df["Cost"].apply(lambda x: f"{x:.2f}" if isinstance(x, (int, float)) else x)
-            df["Time"] = df["Time"].apply(lambda x: f"{x:.2f}" if isinstance(x, (int, float)) else x)
             
-            # Display without index
-            st.write(f'Establishments Found: {data["places"]}')
-            st.table(df)
+                df = pd.DataFrame(table_data)
 
-            # set state variable tracking if a prediction has been generated
-            st.session_state['price_predicted'] = True
-
-            # get the sums of currently selected tiers to display to the user
-            total_prediction = st.session_state['price_prediction']
-            time_prediction = 0
-            cost_prediction = 0
-
-            if st.session_state['basic_data_checkbox']:
-                time_prediction += total_prediction['basic_time']
-                cost_prediction += total_prediction['basic_cost']
-            if st.session_state['include_reviews_checkbox']:
-                time_prediction += total_prediction['reviews_time']
-                cost_prediction += total_prediction['reviews_cost']
-            if st.session_state['include_photo_captioning_checkbox']:
-                time_prediction += total_prediction['photos_time']
-                cost_prediction += total_prediction['photos_cost']
+                # display results
+                st.write('establishments found: -')
+                st.table(df)
             
-            time_prediction = round(time_prediction, 2)
-            cost_prediction = round(cost_prediction, 2)
-
-            query_time_prediction_col, query_cost_prediction_col = st.columns(2)
-
-            # although these are being used below, Im putting them in state so that they can be displayed
-            # on the 'Review + Submit' tab. (This avoids having to format/round the same numbers twice)
-            st.session_state['predicted_time'] = f'{format_duration(time_prediction)}'
-            st.session_state['predicted_cost'] = f'${cost_prediction:.2f}'
-
-            query_time_prediction_col.write(f'Predicted Query Time: {st.session_state['predicted_time']}')
-            query_cost_prediction_col.write(f'Predicted Query Cost: {st.session_state['predicted_cost']}')
-            
-
-
-
-
-            
-    
-
-
 # Ensures the code runs only when this file is executed directly
 if __name__ == "__main__":
     query_options()
