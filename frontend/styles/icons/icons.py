8e52f92443775e749eca75e62829360207f5832e
noland <104016970+xnolandx@users.noreply.github.com>
2025-05-06 10:53:23 -0400

    added icons that are being used for map widgets

--- frontend/styles/icons/icons.py
+++ frontend/styles/icons/icons.py
@@ -4,3 +4,8 @@ validation_error_icon="❌"
 no_results_icon="🔎"
 successful_download="✔️"
 terminated_query_warning="❗"
+
+# search area icons
+pin_icon = "📍"
+bounding_box_icon = "🟦"
+trashcan_icon = "🗑️"
