cf3e58631945cdb6bc7cde32a45b61ecf6d2e63c
noland <104016970+xnolandx@users.noreply.github.com>
2025-05-06 11:08:13 -0400

    removed teh establishment_search function call and placed it in query_options. Changed the app name from 'PLAIDE' to 'PLAID'. updated imports.

--- frontend/app.py
+++ frontend/app.py
@@ -3,14 +3,12 @@ import streamlit as st
 from styles.icons.icons import toolbox_icon
 from state_management.initialize_state import initialize_state
 from tabs.create_tabs import create_tabs
-from tabs.establishment_search import establishment_search
 from tabs.search_area import search_area
 from tabs.query_options import query_options
 from tabs.review_and_submit import review_and_submit
 
-
 st.set_page_config(
-page_title="PLAIDE", 
+page_title="PLAID", 
 page_icon=toolbox_icon,
 layout="wide"
 )
@@ -23,14 +21,13 @@ with open('./styles/styles.css') as f:
       st.markdown(f'<style>{f.read()}<style>', unsafe_allow_html=True)
 
 # Display app name in upper left corner
-st.markdown('<h3>PL<span style="color:red;">AI</span>DE</h3>', unsafe_allow_html=True)
+st.markdown('<h3>PL<span style="color:red;">AI</span>D</h3>', unsafe_allow_html=True)
 
 # define the tabs you want displayed
 active_tab = create_tabs(['Search Area', 'Query Options', 'Review + Submit'])
 
 # checking and conditionally rendering based on what tab is selected
 if st.session_state['active_tab'] == 'Search Area':
-      establishment_search()
       search_area()
 elif st.session_state['active_tab'] == 'Query Options':
       query_options()
